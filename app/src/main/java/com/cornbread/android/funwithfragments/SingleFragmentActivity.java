d7270d08b1f10451218915ea2fb4ee25d4a8291d
DArcos <danarc0s7@gmail.com>
2015-03-08 18:59:45 -0400

    Converted SingleFragmentActivity to host a fragment that will have all UI elements. Replaced background image to smaller size so that it would actually display.

--- app/src/main/java/com/cornbread/android/funwithfragments/SingleFragmentActivity.java
+++ app/src/main/java/com/cornbread/android/funwithfragments/SingleFragmentActivity.java
@@ -3,12 +3,31 @@ package com.cornbread.android.funwithfragments;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class SingleFragmentActivity extends Activity{
+public class SingleFragmentActivity extends FragmentActivity{
+    Button mToastButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.fragment_single);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentContainer);
+
+        if(fragment == null){
+            fragment = new SingleFragment();
+            fragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
     }
 }
