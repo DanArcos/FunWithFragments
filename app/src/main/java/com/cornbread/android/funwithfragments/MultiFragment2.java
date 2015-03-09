a5ec9388052a601a844b861e677c13f421fce354
DArcos <danarc0s7@gmail.com>
2015-03-08 21:38:44 -0400

    Added new button in MultiFragment2 that displays activity name to prove that only one activity is running.

--- app/src/main/java/com/cornbread/android/funwithfragments/MultiFragment2.java
+++ app/src/main/java/com/cornbread/android/funwithfragments/MultiFragment2.java
@@ -3,6 +3,7 @@ package com.cornbread.android.funwithfragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import android.widget.Toast;
 
 public class MultiFragment2 extends Fragment{
     Button mGoBackButton;
+    Button mShowActivity2Button;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -26,6 +28,15 @@ public class MultiFragment2 extends Fragment{
                         .beginTransaction()
                         .replace(R.id.fragmentContainer, new MultiFragment1())
                         .commit();
+            }
+        });
+
+        //Log.d("TAG", getActivity().getClass().getSimpleName());
+        mShowActivity2Button = (Button) v.findViewById(R.id.show_activity2);
+        mShowActivity2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), getActivity().getClass().getSimpleName(), Toast.LENGTH_SHORT).show();
             }
         });
 
